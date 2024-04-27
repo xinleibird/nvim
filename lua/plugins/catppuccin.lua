24bce2a2dc263ca09709a422c917c121a00a7068
辛磊 <xinleibird@gmail.com>
2024-04-28 02:52:08 +0800

    update catppuccin settings

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -89,6 +89,7 @@ local M = {
           -- DevIconTs = { fg = colors.blue },
         }
       end,
+      default_integrations = true,
       integrations = {
         alpha = true,
         barbecue = {
