1eb4ed732d9cc89820077fe0beeea87c6c30d099
辛磊 <xinleibird@gmail.com>
2025-03-31 05:01:30 +0800

    update

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -41,29 +41,7 @@ local M = {
       use_nvim_cmp_as_default = false,
       nerd_font_variant = "mono",
     },
-    cmdline = {
-      enabled = false,
-      sources = function()
-        local type = vim.fn.getcmdtype()
-        -- Search forward and backward
-        if type == "/" or type == "?" then
-          return { "buffer" }
-        end
-        -- Commands
-        if type == ":" or type == "@" then
-          return { "cmdline" }
-        end
-        return {}
-      end,
-      keymap = {
-        ["<CR>"] = { "accept_and_enter", "fallback" },
-      },
-      -- completion = { menu = { auto_show = true } },
-    },
     sources = {
-      min_keyword_length = function()
-        return vim.bo.filetype == "markdown" and 2 or 0
-      end,
       -- default = { "lsp", "path", "snippets", "buffer" },
       default = function()
         local success, node = pcall(vim.treesitter.get_node)
@@ -77,6 +55,9 @@ local M = {
           return { "lsp", "path", "snippets", "buffer" }
         end
       end,
+      min_keyword_length = function()
+        return vim.bo.filetype == "markdown" and 2 or 0
+      end,
       providers = {
         lsp = {
           transform_items = function(_, items)
@@ -88,16 +69,9 @@ local M = {
               return true
             end, items)
           end,
-          override = {
-            get_trigger_characters = function(self)
-              local trigger_characters = self:get_trigger_characters()
-              trigger_characters = vim.tbl_filter(function(char)
-                -- for emmet_language_server disable {, [, ( trigger
-                return char ~= "}" and char ~= "]" and char ~= ")"
-              end, trigger_characters)
-              return trigger_characters
-            end,
-          },
+          should_show_items = function(ctx)
+            return ctx.trigger.initial_kind ~= "trigger_character"
+          end,
         },
         path = {
           opts = {
@@ -153,6 +127,19 @@ local M = {
         },
       },
     },
+    completion = {
+      -- trigger = {
+      --   show_on_insert_on_trigger_character = false,
+      --   show_on_accept_on_trigger_character = false,
+      --   show_on_x_blocked_trigger_characters = {},
+      -- },
+      menu = {
+        draw = {
+          -- show completion kind
+          columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+        },
+      },
+    },
     fuzzy = {
       implementation = "prefer_rust_with_warning",
       sorts = {
@@ -169,13 +156,24 @@ local M = {
         "sort_text",
       },
     },
-    completion = {
-      menu = {
-        draw = {
-          -- show completion kind
-          columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
-        },
+    cmdline = {
+      -- enabled = false,
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Search forward and backward
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        -- Commands
+        if type == ":" or type == "@" then
+          return { "cmdline" }
+        end
+        return {}
+      end,
+      keymap = {
+        ["<CR>"] = { "accept_and_enter", "fallback" },
       },
+      -- completion = { menu = { auto_show = true } },
     },
   },
   opts_extend = { "sources.default" },
