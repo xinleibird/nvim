2b3d44559128bcc41985899d0992dae266b727f1
辛磊 <xinleibird@gmail.com>
2025-05-10 23:18:20 +0800

    add fidget lsp progress for codecompanion

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,16 @@
+local M = {
+  "j-hui/fidget.nvim",
+  event = "LspAttach",
+  opts = {
+    progress = {
+      display = {
+        render_limit = 16, -- How many LSP messages to show at once
+        done_ttl = 1, -- How long a message should persist after completion
+        done_icon = require("configs.icons").ui.CheckBold, -- Icon shown when all LSP progress tasks are complete
+        progress_icon = { pattern = "circle_halves", period = 1 },
+      },
+    },
+  },
+}
+
+return M
