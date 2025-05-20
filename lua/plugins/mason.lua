90c951c20be7fce241af36d10001b4d7092ad0db
辛磊 <xinleibird@gmail.com>
2025-05-20 21:28:34 +0800

    update MasonInstallAll command

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,50 +2,66 @@ local M = {
   "williamboman/mason.nvim",
   cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
   init = function()
-    vim.g.mason_ensure_installed = {
-      -- lua stuff
-      "lua-language-server",
-      "stylua",
-
-      -- web dev stuff
-      "css-lsp",
-      "eslint-lsp",
-      "emmet-language-server",
-      "html-lsp",
-      "htmlhint",
-      "prettier",
-      "svelte-language-server",
-      "typescript-language-server",
-      "vtsls",
-      "vue-language-server",
-
-      -- browser debug
-      "firefox-debug-adapter",
-      "js-debug-adapter",
-
-      -- shell stuff
-      "bash-language-server",
-      "shfmt",
-      "shellcheck",
-
-      -- rust stuff
-      "rust-analyzer",
-      "codelldb",
-
-      -- vim stuff
-      "vim-language-server",
-
-      -- json / yaml
-      "json-lsp",
-      "yaml-language-server",
-
-      -- markdown
-      "marksman",
-    }
     -- custom cmd to install all mason binaries listed
     vim.api.nvim_create_user_command("MasonInstallAll", function()
-      if vim.g.mason_ensure_installed and #vim.g.mason_ensure_installed > 0 then
-        vim.cmd("MasonInstall " .. table.concat(vim.g.mason_ensure_installed, " "))
+      local mason_ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev stuff
+        "css-lsp",
+        "eslint-lsp",
+        "emmet-language-server",
+        "html-lsp",
+        "htmlhint",
+        "prettier",
+        "svelte-language-server",
+        "typescript-language-server",
+        "vtsls",
+        "vue-language-server",
+
+        -- browser debug
+        "firefox-debug-adapter",
+        "js-debug-adapter",
+
+        -- shell stuff
+        "bash-language-server",
+        "shfmt",
+        "shellcheck",
+
+        -- rust stuff
+        "rust-analyzer",
+        "codelldb",
+
+        -- vim stuff
+        "vim-language-server",
+
+        -- json / yaml
+        "json-lsp",
+        "yaml-language-server",
+
+        -- markdown
+        "marksman",
+      }
+      local mason_already_installed = require("mason-registry").get_installed_package_names()
+
+      local already_set = {}
+      for _, package_name in pairs(mason_already_installed) do
+        already_set[package_name] = true
+      end
+
+      local will_install = {}
+      for _, package_name in ipairs(mason_ensure_installed) do
+        if not already_set[package_name] then
+          table.insert(will_install, package_name)
+        end
+      end
+
+      if #will_install > 0 then
+        vim.cmd("MasonInstall " .. table.concat(will_install, " "))
+      else
+        vim.notify("All mason packages have been installed.", vim.log.levels.INFO, { title = "Mason" })
       end
     end, {})
   end,
@@ -54,8 +70,6 @@ local M = {
     local icons = require("configs.icons")
 
     return {
-      ensure_installed = vim.g.mason_ensure_installed,
-
       ---@type '"prepend"' | '"append"' | '"skip"'
       PATH = "prepend",
 
