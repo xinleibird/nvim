aa7d36d67492910db15e703fdca1b5a509034e42
辛磊 <xinleibird@gmail.com>
2025-05-24 03:46:06 +0800

    update flash.nvim behavior for operator-pending mode

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -43,7 +43,7 @@ vim.o.timeoutlen = 400
 vim.o.undofile = true
 
 -- interval for writing swap file to disk, also used by gitsigns
-vim.o.updatetime = 100
+vim.o.updatetime = 200
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -18,8 +18,19 @@ local M = {
     },
     modes = {
       char = {
-        enabled = true,
+        config = function(opts)
+          -- autohide flash when in operator-pending mode
+          opts.autohide = opts.autohide or (vim.fn.mode(true):find("no") and vim.v.operator == "y")
+          opts.multi_line = opts.multi_line and not vim.fn.mode(true):find("o")
+
+          opts.jump_labels = opts.jump_labels
+            and not vim.fn.mode(true):find("o") -- hide jump_labels in operator pending mode
+            and vim.v.count == 0
+            and vim.fn.reg_executing() == ""
+            and vim.fn.reg_recording() == ""
+        end,
         autohide = true,
+        search = { wrap = true },
         multi_line = false,
         jump_labels = true,
         keys = { "f", "F", "t", "T" },
