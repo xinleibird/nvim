c513aa00088d854772dff1bfa968f485274d9b9c
辛磊 <xinleibird@gmail.com>
2025-05-27 10:50:59 +0800

    enabled treesitter support for codecompanion

--- lsp/marksman.lua
+++ lsp/marksman.lua
@@ -1,6 +1,6 @@
 return {
   cmd = { "marksman", "server" },
-  filetypes = { "markdown", "markdown.mdx" },
+  filetypes = { "markdown", "markdown.mdx", "codecompanion" },
   root_markers = { ".marksman.toml", ".git" },
   single_file_support = true,
 }
--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,16 +1,13 @@
 local M = {
-  -- Make sure to set this up properly if you have lazy=true
-  "MeanderingProgrammer/render-markdown.nvim",
-  opts = {
-    file_types = { "markdown", "Avante", "codecompanion" },
-  },
-  ft = { "markdown", "Avante", "codecompanion" },
-  config = function()
-    require("render-markdown").setup({
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" },
+    opts = {
+      file_types = { "markdown", "codecompanion" },
       latex = { enabled = false },
       completions = { blink = { enabled = true } },
-    })
-  end,
+    },
+  },
 }
 
 return M
