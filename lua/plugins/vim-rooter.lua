bd3390cd4c4f5566e5ea3886d7c5ee31821d8365
辛磊 <xinleibird@gmail.com>
2026-01-14 22:40:44 +0800

    add rooter sync file

--- lua/plugins/vim-rooter.lua
+++ lua/plugins/vim-rooter.lua
@@ -92,8 +92,20 @@ local M = {
 
     -- a literal / to match directory buffers, a star * to match file buffers. disable directory buffers for snacks.explorer. default is {"/", "*"}
     vim.g.rooter_targets = { "*" }
-
     vim.g.rooter_silent_chdir = 1
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "RooterChDir",
+      group = vim.api.nvim_create_augroup("user_rooter_chdir", { clear = true }),
+      callback = function()
+        local cwd = vim.fn.getcwd()
+        local file = io.open(vim.fn.stdpath("cache") .. "/rooter_last_dir", "w")
+        if file then
+          file:write(cwd)
+          file:close()
+        end
+      end,
+    })
   end,
 }
 
