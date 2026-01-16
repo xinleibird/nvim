8f1978995f0102d835fef2cbc2535bca5cf82c1e
辛磊 <xinleibird@gmail.com>
2026-01-16 21:20:32 +0800

    add mind.nvim

---
+++ lua/plugins/mind.lua
@@ -0,0 +1,13 @@
+local M = {
+  "Selyss/mind.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- optional, used for icons
+  },
+  opts = {
+    -- your configuration comes here
+  },
+}
+
+return M
