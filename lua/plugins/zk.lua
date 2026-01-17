86405f4dfb3076d3c82caa5c60b0f245442cd25d
辛磊 <xinleibird@gmail.com>
2026-01-18 06:03:17 +0800

    add zk-nvim keymap

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -1,6 +1,11 @@
 local M = {
   "zk-org/zk-nvim",
   lazy = false,
+  init = function()
+    vim.keymap.set("n", "<leader>zp", "<cmd>ZkNotes<cr>", { desc = "Zk Notes" })
+    vim.keymap.set("n", "<leader>zt", "<cmd>ZkTags<cr>", { desc = "Zk Tags" })
+    vim.keymap.set("n", "<leader>zn", "<cmd>ZkNew<cr>", { desc = "Zk New" })
+  end,
   config = function()
     require("zk").setup({
       picker = "snacks_picker",
