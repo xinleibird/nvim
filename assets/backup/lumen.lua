750e215724ac5102e8eef4ebf8306aaa93192b5c
辛磊 <xinleibird@gmail.com>
2026-01-27 08:38:15 +0800

    chore(backup): remove legacy neovim plugin configurations

--- assets/backup/lumen.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-  -- make sure install this, it guarantees that vim.o.background can be obtained quickly
-  -- and that some GUI programs can obtain background values.
-  -- https://github.com/neovim/neovim/pull/31350, neovim v10 supported, but not perfect.
-  "vimpostor/vim-lumen",
-  lazy = false,
-}
-
-return M
