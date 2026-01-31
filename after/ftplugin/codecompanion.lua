bf11a9ab3bbea5e64bb298394c37803abb6eef79
辛磊 <xinleibird@gmail.com>
2026-01-31 21:16:39 +0800

    style(codecompanion): enable line wrap and disable cursor concealment

--- after/ftplugin/codecompanion.lua
+++ after/ftplugin/codecompanion.lua
@@ -1,7 +1,8 @@
-vim.opt_local.wrap = false
+vim.opt_local.wrap = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
+vim.opt_local.concealcursor = ""
 
 vim.b.snacks_scope = false
 vim.b.snacks_image_attached = true
