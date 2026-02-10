7290915df9d25893986886778ab45a4645a779cf
辛磊 <xinleibird@gmail.com>
2026-02-10 19:46:25 +0800

    feat(lsp): add cssls configuration for css, scss, and less support

---
+++ after/lsp/cssls.lua
@@ -0,0 +1,21 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "vscode-css-language-server", "--stdio" },
+  filetypes = { "css", "scss", "less" },
+  init_options = { provideFormatter = true }, -- needed to enable formatting capabilities
+  root_markers = { "package.json", ".git" },
+  settings = {
+    css = {
+      validate = true,
+      lint = { unknownAtRules = "ignore" },
+    },
+    scss = {
+      validate = true,
+      lint = { unknownAtRules = "ignore" },
+    },
+    less = {
+      validate = true,
+      lint = { unknownAtRules = "ignore" },
+    },
+  },
+}
