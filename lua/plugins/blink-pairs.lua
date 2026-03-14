37a7d51d74d9f7496ad8cdc8b30783f5c6f99f00
辛磊 <xinleibird@gmail.com>
2026-03-15 04:09:28 +0800

    build(blink.pairs): switch from prebuilt binaries to source build

--- lua/plugins/blink-pairs.lua
+++ lua/plugins/blink-pairs.lua
@@ -2,14 +2,14 @@
 ---@type LazySpec
 local M = {
   "saghen/blink.pairs",
-  version = "*", -- (recommended) only required with prebuilt binaries
+  -- version = "*", -- (recommended) only required with prebuilt binaries
   lazy = false,
 
   -- download prebuilt binaries from github releases
-  dependencies = "saghen/blink.download",
+  -- dependencies = "saghen/blink.download",
   -- OR build from source, requires nightly:
   -- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
+  build = "cargo build --release",
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
 
