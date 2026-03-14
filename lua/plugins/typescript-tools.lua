954d59532259b23bfea260f8f67381edf4116c6f
辛磊 <xinleibird@gmail.com>
2026-03-14 09:52:39 +0800

    chore: remove ts-error-translator.nvim plugin configuration

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -5,22 +5,6 @@ local M = {
   event = "VeryLazy",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    {
-      "dmmulroy/ts-error-translator.nvim",
-      config = function()
-        require("ts-error-translator").setup({
-          auto_attach = true,
-          servers = {
-            "astro",
-            "svelte",
-            "ts_ls",
-            "typescript-tools",
-            "volar",
-            "vtsls",
-          },
-        })
-      end,
-    },
   },
   opts = {
     settings = {
