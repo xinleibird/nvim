095cb6e706aecc2a117b1056ca5173e40e13a99b
辛磊 <xinleibird@gmail.com>
2026-03-15 06:49:50 +0800

    feat(blink-pairs): add cmdline-aware auto-pairing and markdown code fence support

--- lua/plugins/blink-pairs.lua
+++ lua/plugins/blink-pairs.lua
@@ -13,6 +13,27 @@ local M = {
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
 
+  init = function()
+    vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
+      group = vim.api.nvim_create_augroup("user_cmdline_enter", { clear = true }),
+      callback = function()
+        local type = vim.fn.getcmdtype()
+        if type == "/" or type == "?" then
+          vim.b.blink_pairs = false
+        end
+        if type == ":" or type == "@" then
+          vim.b.blink_pairs = true
+        end
+      end,
+    })
+    vim.api.nvim_create_autocmd({ "CmdlineLeave" }, {
+      group = vim.api.nvim_create_augroup("user_cmdline_leave", { clear = true }),
+      callback = function()
+        vim.b.blink_pairs = true
+      end,
+    })
+  end,
+
   --- @module 'blink.pairs'
   --- @type blink.pairs.Config
   opts = {
@@ -24,10 +45,35 @@ local M = {
       cmdline = true,
       -- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
       -- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
-      disabled_filetypes = {},
+      disabled_filetypes = { "snacks_picker_input" },
       -- see the defaults:
       -- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L14
-      pairs = {},
+      pairs = {
+        ["`"] = {
+          {
+            "```",
+            when = function(ctx)
+              return ctx:text_before_cursor(2) == "``"
+            end,
+            languages = { "markdown", "markdown_inline", "typst", "vimwiki", "rmarkdown", "rmd", "quarto" },
+            space = false,
+          },
+          {
+            " ",
+            "```",
+            when = function(ctx)
+              return (ctx:text_before_cursor(nil)):match("^```.*$")
+            end,
+            languages = { "markdown", "markdown_inline", "typst", "vimwiki", "rmarkdown", "rmd", "quarto" },
+          },
+          {
+            "`",
+            "'",
+            languages = { "bibtex", "latex", "plaintex" },
+          },
+          { "`", enter = false, space = false },
+        },
+      },
     },
     highlights = {
       enabled = true,
