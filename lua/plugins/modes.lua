49eb11a9b8812b170b82a684b9de7c00668b0bce
辛磊 <xinleibird@gmail.com>
2026-03-31 03:57:14 +0800

    feat(modes): add ignore filetypes configuration for snacks_picker and codecompanion

--- lua/plugins/modes.lua
+++ lua/plugins/modes.lua
@@ -2,11 +2,24 @@
 ---@type LazySpec
 local M = {
   "mvllow/modes.nvim",
-  event = { "BufRead", "BufNewFile", "User SnacksDashboardflavourlosed" },
+  event = { "BufRead", "BufNewFile", "User SnacksDashboardClosed", "User SnacksDashboardOpened" },
   config = function()
     require("modes").setup({
       line_opacity = 0.15,
       set_cursorline = false,
+      -- Disable modes highlights for specified filetypes
+      -- or enable with prefix "!" if otherwise disabled (please PR common patterns)
+      -- Can also be a function fun():boolean that disables modes highlights when true
+      ignore = {
+        "lspinfo",
+        "checkhealth",
+        "help",
+        "man",
+        "!snacks_picker_list",
+        "!snacks_picker_input",
+        "!codecompanion",
+        "Outline",
+      },
     })
   end,
 }
