46657ac92adb77d3905a07a846169e7c411411ec
辛磊 <xinleibird@gmail.com>
2026-05-06 06:54:33 +0800

    feat(jsonls): customize jsonls schemas with schemastore select/replace

--- after/lsp/jsonls.lua
+++ after/lsp/jsonls.lua
@@ -3,7 +3,24 @@ return {
   single_file_support = true,
   settings = {
     json = {
-      schemas = require("schemastore").json.schemas(),
+      schemas = require("schemastore").json.schemas({
+        select = {
+          ".eslintrc",
+          "lerna.json",
+          "package.json",
+          "prettierrc.json",
+          "tsconfig.json",
+          "tslint.json",
+        },
+        replace = {
+          ["tsconfig.json"] = {
+            description = "Custom JSON schema for typescript configuration files",
+            fileMatch = { "tsconfig*.json" },
+            name = "tsconfig.json",
+            url = "https://json.schemastore.org/tsconfig.json",
+          },
+        },
+      }),
       validate = { enable = true },
     },
   },
